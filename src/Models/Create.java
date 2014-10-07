ed588a5870da3217b9d950b0df2d93f1487fe4e0
Chrucas <Christiaan.walters@gmail.com>
2014-10-07 16:07:43 +0200

    Insertion Sort toegevoegd en wat netter gemaakt

--- src/Models/Create.java
+++ src/Models/Create.java
@@ -16,21 +16,34 @@ public class Create {
     public Create(int size){
     Student[]SList = new Student[size];
     for(int i = 0;i<i+size;i++){
-       SList[i]= new Student();
-       
+       SList[i]= new Student();  
     }    
     this.size=size;    
     }
     public void initialize(Student[] SList){
        Random ran = new Random();
        for(int i=1;i<i+SList.length;i++){
+           double grade = (ran.nextInt(9)+1)+ran.nextDouble()*10+1;
+           if (grade>10.0){
+                grade= 10.0;
+           }
+           SList[i].setGrade(grade);
+
            for(int j=50060001;j<j+size;j++)
            SList[i].setStudentNumber(j);
-           double grade = (ran.nextInt(9)+1)+ran.nextDouble()*10+1;
-           SList[i].setGrade(grade);
-           if (grade>10.0){
-            grade= 10.0;
-            }
+           
+           SList[i].setGroup(null);
        }
     }
-}
+    
+    public void InsSortGrade(Student[] SList){
+        for(int i=1;i<SList.length;i++){
+            Student temp = SList[i];
+            int j;
+            for(j=i-1;j>=0&&temp.getGrade()<SList[j].getGrade(); j--){
+                SList[j+1]=SList[j];
+            }
+            SList[j+1]= temp;
+        }
+    }
+}    
